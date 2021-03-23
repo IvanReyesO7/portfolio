0f060125a02c8891ed8d1b8b97697075275fb962
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-23 18:37:22 +0900

    advanced on the first two lines

--- source/assets/javascripts/text.js
+++ source/assets/javascripts/text.js
@@ -2,16 +2,33 @@ import Typed from 'typed.js';
 
 const terminalText = () => {
   const firstLine = () => {
-    new Typed('#first-line', {
-      strings: ["", " Ivan Reyes"],
-      typeSpeed: 50,
-      loop: false
-    });
+      new Typed('#first-line', {
+        strings: ["", " Ivan Reyes"],
+        typeSpeed: 50,
+        loop: false,
+        showCursor: false
+      });
   };
+
+
+  const guest = (callback) => {
+    setTimeout(function(){
+      document.querySelector('#guest2').hidden = false;
+      callback();
+    },2000);
+  };
+
+  const secondLine = () => {
+      new Typed('#second-line', {
+        strings: ["", " Full-Stack developer"],
+        typeSpeed: 50,
+        loop: false,
+        showCursor: false
+      });
+  };
+
   firstLine();
-  const texto = document.querySelector("#guest2");
-  console.log(texto);
-  texto.hidden = false
+  guest(secondLine);
 }
 
 export { terminalText };
